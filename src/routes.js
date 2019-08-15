9f025c7bb8ad6c72c31a8426330b2ccc323ced44
Dyarlen Iber <dyarlen1@gmail.com>
2019-08-14 23:28:25 -0300

    Ads CRUD and paginate

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,22 @@ const express = require('express')
 
 const routes = express.Router()
 
-const authMiddlewares = require('./app/middlewares/auth')
+const authMiddleware = require('./app/middlewares/auth')
 
 const controllers = require('./app/controllers')
 
 routes.post('/users', controllers.UserController.store)
 routes.post('/sessions', controllers.SessionController.store)
 
-routes.get('/test', authMiddlewares, (req, res) => res.json({ ok: true }))
+routes.use(authMiddleware)
+
+/**
+ * Ads
+ */
+routes.get('/ads', controllers.AdController.index)
+routes.get('/ads/:id', controllers.AdController.show)
+routes.post('/ads', controllers.AdController.store)
+routes.put('/ads/:id', controllers.AdController.update)
+routes.delete('/ads/:id', controllers.AdController.destroy)
 
 module.exports = routes
